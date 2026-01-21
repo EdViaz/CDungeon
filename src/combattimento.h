731b3dbccc9f3d160577e248c17cc9cfe5e2992c
EdViaz <edoardo.viale.06@gmail.com>
2026-01-21 13:35:10 +0100

    a

--- src/combattimento.h
+++ src/combattimento.h
@@ -1,13 +1,11 @@
 #ifndef COMBATTIMENTO_H
 #define COMBATTIMENTO_H
 
-#include "dungeon.h"
 #include "stanza.h"
 #include "giocatore.h"
 
 int inizia_combattimento(Giocatore *g, Stanza *nemico);
 void applica_danno_trappola(Giocatore *g, Stanza *trappola);
-int combattimento_boss_finale(
-    Giocatore *g); // Ritorna 1 se il giocatore vince, 0 se muore
+int combattimento_boss_finale(Giocatore *g); // Ritorna 1 se il giocatore vince, 0 se muore
 
 #endif
